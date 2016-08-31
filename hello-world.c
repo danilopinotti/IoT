278ce6e4d562dbf5b4c774b6e3a3fd3e07bd7cc4
Danilo Pinotti <danilopinotti@hotmail.com>
2016-08-31 18:11:30 -0300

    maybe

--- hello-world.c
+++ hello-world.c
@@ -1,34 +1,3 @@
-/*
- * Copyright (c) 2006, Swedish Institute of Computer Science.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file is part of the Contiki operating system.
- *
- */
 
 /**
  * \file
@@ -41,6 +10,7 @@
 #include "dev/leds.h"
 #include "dev/light-sensor.h"
 #include "dev/button-sensor.h"
+#include "sys/etimer.h"
 
 #include <stdio.h> /* For printf() */
 /*---------------------------------------------------------------------------*/
@@ -55,13 +25,23 @@ PROCESS_THREAD(hello_world_process, ev, data)
   SENSORS_ACTIVATE(light_sensor);
   
   static int counter = 0;
+  static struct etimer timer;
+  etimer_set(&timer, CLOCK_SECOND*4);
 
   while(1){
-   PROCESS_WAIT_EVENT();
-   if(ev == sensors_event && data == &button_sensor){
+
+   PROCESS_WAIT_EVENT_UNTIL(ev == sensors_event && data == &button_sensor);
      counter++;
+     printf("Button press!");
+     leds_toggle(LEDS_ALL);
      printf("Light: \%u\nCounter: \%u\n", light_sensor.value(0), counter);
-   }
+
+
+   PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
+     leds_toggle(LEDS_ALL);
+     etimer_reset(&timer);
+     printf("Timer !\n");
+
   }
 
   PROCESS_END();
