95fd67edbdfcb2f7b9da25d99040c49b600bc63f
Danilo Pinotti <danilopinotti@hotmail.com>
2016-09-15 21:30:32 -0300

    Configurate IPv6

--- atividade_2/src/receiver.c
+++ atividade_2/src/receiver.c
@@ -2,6 +2,10 @@
 
 #include <stdio.h>
 
+#include "net/ip/uip.h"
+
+#include "net/ipv6/uip-ds6.h"
+
 PROCESS(skel_process, "Skel process");
 
 AUTOSTART_PROCESSES(&skel_process);
@@ -11,6 +15,12 @@ PROCESS_THREAD(skel_process, ev, data)
 {
 
   PROCESS_BEGIN();
+
+  static uip_ipaddr_t ipaddr;
+
+  uip_ip6addr(&ipaddr, 0xcafe, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x2);
+
+  uip_ds6_addr_add(&ipaddr, 0, ADDR_MANUAL); 
 
   printf ("Receiver funcionando!\n");
 
