dcdaad016f97982b753ceb464c6ec6780a5b11b4
Danilo Pinotti <danilopinotti@hotmail.com>
2016-12-01 18:02:52 -0200

    Mote CoAP quase funcionando

--- trabalho_4/www/scripts/mote.py
+++ trabalho_4/www/scripts/mote.py
@@ -1,35 +1,55 @@
-import socket
-import sys
 import time
+import logging
+import asyncio
+from aiocoap import *
+
+logging.basicConfig(level=logging.INFO)
+
+@asyncio.coroutine
+
+def getStatusExec(s_id, future):
+  s_id = str(s_id)
+  protocol = yield from Context.create_client_context()
+  request = Message(code=GET)
+  request.set_request_uri('coap://[cafe::c30c:0:0:'+s_id+']/actuators/ledstatus')
+  try:
+    response = yield from protocol.request(request).response
+  except Exception as e:
+    print('Failed to fetch resource:')
+    print(e)
+  else:
+    #print('Result: %s\n%r'%(response.code, response.payload))
+    future.set_result(response.payload)
 
 def getStatus(s_id):
-  s_id = str(s_id)
-  address = "cafe::212:740"+s_id+":"+s_id+":"+s_id+"0"+s_id 
-  port = 5000
-  message = str.encode('STATUS')
-  sock = socket.socket(socket.AF_INET6,socket.SOCK_DGRAM)
-  sock2 = socket.socket(socket.AF_INET6,socket.SOCK_DGRAM)
-  sock2.bind(("cafe::1", 5000))
-  sock.sendto(message, (address, port))
-  (data, addr) = sock2.recvfrom(1024)
-  return data.decode()
+  future = asyncio.Future()
+  asyncio.ensure_future(getStatusExec(s_id, future))
+  asyncio.get_event_loop().run_until_complete(future)
+  print("RESULTADO: "+future.result())
+  return future.result()
 
 def toggle(s_id):
+  asyncio.get_event_loop().run_until_complete(toggleExec(s_id))
+
+def toggleExec(s_id):
   s_id = str(s_id)
-  address = "cafe::212:740"+s_id+":"+s_id+":"+s_id+"0"+s_id
-  port = 5000
-  message = str.encode('TOGGLE')
-  sock = socket.socket(socket.AF_INET6,socket.SOCK_DGRAM)
-  sock.sendto(message, (address, port))
+  protocol = yield from Context.create_client_context()
+  request = Message(code=POST)
+  request.set_request_uri('coap://[cafe::c30c:0:0:'+s_id+']/actuators/toggle')
+  try:
+    response = yield from protocol.request(request).response
+  except Exception as e:
+    print('Failed to fetch resource:')
+    print(e)
+  else:
+    print('Result: %s\n%r'%(response.code, response.payload))
 
 def turnOff(s_id):
-  time.sleep(0.1)
   if getStatus(s_id) == "ON":
     time.sleep(0.1)
     toggle(s_id)
 
 def turnOn(s_id):
-  time.sleep(0.1)
   if getStatus(s_id) == "OFF":
     time.sleep(0.1)
     toggle(s_id)
