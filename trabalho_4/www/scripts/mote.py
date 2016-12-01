7a6d13239a1d0aff1d440a190d1b475a80f1685c
Danilo Pinotti <danilopinotti@hotmail.com>
2016-12-01 15:13:00 -0200

    Inicio do trabalho 4

---
+++ trabalho_4/www/scripts/mote.py
@@ -0,0 +1,36 @@
+import socket
+import sys
+import time
+
+def getStatus(s_id):
+  s_id = str(s_id)
+  address = "cafe::212:740"+s_id+":"+s_id+":"+s_id+"0"+s_id 
+  port = 5000
+  message = str.encode('STATUS')
+  sock = socket.socket(socket.AF_INET6,socket.SOCK_DGRAM)
+  sock2 = socket.socket(socket.AF_INET6,socket.SOCK_DGRAM)
+  sock2.bind(("cafe::1", 5000))
+  sock.sendto(message, (address, port))
+  (data, addr) = sock2.recvfrom(1024)
+  return data.decode()
+
+def toggle(s_id):
+  s_id = str(s_id)
+  address = "cafe::212:740"+s_id+":"+s_id+":"+s_id+"0"+s_id
+  port = 5000
+  message = str.encode('TOGGLE')
+  sock = socket.socket(socket.AF_INET6,socket.SOCK_DGRAM)
+  sock.sendto(message, (address, port))
+
+def turnOff(s_id):
+  time.sleep(0.1)
+  if getStatus(s_id) == "ON":
+    time.sleep(0.1)
+    toggle(s_id)
+
+def turnOn(s_id):
+  time.sleep(0.1)
+  if getStatus(s_id) == "OFF":
+    time.sleep(0.1)
+    toggle(s_id)
+  
